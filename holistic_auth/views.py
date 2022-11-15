413009f19fb5e99411169299f8f79e012cff6352
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-15 20:29:23 +0700

    Introduces `AccountView` - A view to create a new account

--- holistic_auth/views.py
+++ holistic_auth/views.py
@@ -1,7 +1,13 @@
 from knox import views as knox_views
+from drf_rw_serializers import generics
+from rest_framework.permissions import AllowAny
 from rest_framework.authentication import BasicAuthentication
 
 from holistic_auth.auth import TokenAuthentication
+from holistic_auth.serializers import (
+    UserSerializer,
+    UserDeserializer
+)
 
 
 class LoginView(knox_views.LoginView):
@@ -14,3 +20,9 @@ class LogoutView(knox_views.LogoutView):
 
 class LogoutAllView(knox_views.LogoutAllView):
     authentication_classes = [TokenAuthentication]
+
+
+class AccountView(generics.CreateAPIView):
+    read_serializer_class = UserSerializer
+    write_serializer_class = UserDeserializer
+    permission_classes = [AllowAny]
