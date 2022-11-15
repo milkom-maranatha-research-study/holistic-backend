a970dc6b83ef616bb5b884dc8a0a0f0fcdbd5679
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-15 16:17:17 +0700

    Introduces the `holistic_auth` application - Introduce `User` data models - Introduce admin page to manage `User` objects

---
+++ holistic_auth/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'is_service_account', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_service_account')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_service_account', 'groups')
---
+++ holistic_auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HolisticAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'holistic_auth'
