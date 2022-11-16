1cb4ecf1c592126a62cb77090f3cc459ce0bec9e
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-16 14:17:09 +0700

    Introduces update user deserializer and its tests

--- holistic_auth/serializers.py
+++ holistic_auth/serializers.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from django.db.models import Q
 from django.db.utils import IntegrityError
 from django.contrib.auth import get_user_model
@@ -25,7 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
         read_only = fields
 
 
-class UserDeserializer(serializers.Serializer):
+class UserCreateDeserializer(serializers.Serializer):
     username = serializers.CharField(max_length=128)
     email = serializers.EmailField()
     password = serializers.CharField(max_length=128)
@@ -60,3 +61,59 @@ class UserDeserializer(serializers.Serializer):
             raise ValidationError('Unable to create account.')
 
         return user
+
+
+class UserUpdateDeserializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(max_length=128, required=False)
+    new_password = serializers.CharField(max_length=128, required=False)
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'old_password', 'new_password',)
+
+    def validate(self, data):
+        # Validate action of updating password
+        new_password = data.get('new_password')
+
+        if not new_password:
+            return data
+
+        old_password = data.get('old_password')
+
+        # Ensures old password is valid
+        if not old_password:
+            raise ValidationError('\'old_password\' is required to change your password.')
+
+        if not self.instance.check_password(old_password):
+            raise ValidationError('You old password is incorrect.')
+
+        # Ensures the new password is valid
+        instance = deepcopy(self.instance)
+        instance.first_name = data.get('first_name')
+        instance.last_name = data.get('last_name')
+        instance.email = data.get('email')
+
+        # Execute Django password validation
+        validate_password(password=new_password, user=instance)
+
+        return data
+
+    def validate_email(self, value):
+        if User.objects.filter(email__iexact=value).exists():
+            raise ValidationError('Email is already exists!')
+
+        return value
+
+    def update(self, instance, validated_data):
+        new_password = validated_data.pop('new_password', None)
+
+        if hasattr(validated_data, 'old_password'):
+            del validated_data['old_password']
+
+        instance = super().update(instance, validated_data)
+
+        if new_password:
+            instance.set_password(new_password)
+            instance.save()
+
+        return instance
--- holistic_auth/tests/test_serializers.py
+++ holistic_auth/tests/test_serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from django.core.exceptions import ValidationError as DjangoValidationError
 from model_bakery import baker
 from rest_framework.exceptions import ErrorDetail, ValidationError
@@ -6,7 +7,8 @@ from rest_framework.test import APIRequestFactory, APITestCase
 
 from holistic_auth.serializers import (
     UserSerializer,
-    UserDeserializer,
+    UserCreateDeserializer,
+    UserUpdateDeserializer,
 )
 
 
@@ -39,14 +41,14 @@ class TestUserSerializer(APITestCase):
         self.assertDictEqual(expected, actual)
 
 
-class TestUserDeserializer(APITestCase):
+class TestUserCreateDeserializer(APITestCase):
     """
-    Test the `UserDeserializer`
+    Test the `UserCreateDeserializer`
     """
 
     def setUp(self):
         self.factory = APIRequestFactory()
-        self.deserializer = UserDeserializer
+        self.deserializer = UserCreateDeserializer
 
     def test_all_params(self):
         """
@@ -77,7 +79,7 @@ class TestUserDeserializer(APITestCase):
         self.assertIsNotNone(instance.date_joined)
         self.assertFalse(instance.is_active)
 
-    def test_missing_params(self):
+    def test_missing_all_params(self):
         """
         Test the deserializer with missing all params
         """
@@ -172,7 +174,8 @@ class TestUserDeserializer(APITestCase):
             deserializer.validate(data)
 
         self.assertEqual(
-            error.exception.detail, [
+            error.exception.detail,
+            [
                 ErrorDetail(string='Username/Email is already exists!', code='invalid')
             ]
         )
@@ -198,7 +201,8 @@ class TestUserDeserializer(APITestCase):
             deserializer.validate(data)
 
         self.assertEqual(
-            error.exception.detail, [
+            error.exception.detail,
+            [
                 ErrorDetail(string='Username/Email is already exists!', code='invalid')
             ]
         )
@@ -315,4 +319,280 @@ class TestUserDeserializer(APITestCase):
         self.assertListEqual(
             error.exception.messages,
             ['The password is too similar to the last name.']
+        )
+
+
+class TestUserUpdateDeserializer(APITestCase):
+    """
+    Test the `UserUpdateDeserializer`
+    """
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.deserializer = UserUpdateDeserializer
+
+    def test_all_params(self):
+        """
+        Test the deserializer with complete and valid parameters
+        """
+        old_password = 'ValidSecretP4ssw0rd!'
+
+        instance = baker.make(User)
+        instance.password = make_password(old_password)
+        instance.save()
+
+        data = {
+            'email': 'valid@mail.com',
+            'old_password': old_password,
+            'new_password': 'SuperSecretP4ssw0rd',
+            'first_name': 'Abra',
+            'last_name': 'Cadabra'
+        }
+
+        deserializer = self.deserializer(instance=instance, data=data)
+
+        try:
+            deserializer.is_valid(raise_exception=True)
+        except ValidationError:
+            self.fail('Boom! You fail the test.')
+
+        instance = deserializer.save()
+
+        self.assertEqual(instance.email, data['email'])
+        self.assertEqual(instance.first_name, data['first_name'])
+        self.assertEqual(instance.last_name, data['last_name'])
+
+    def test_missing_all_params(self):
+        """
+        Test the deserializer with missing all params
+        """
+        instance = baker.make(User)
+
+        data = {}
+
+        deserializer = self.deserializer(instance=instance, data=data)
+
+        try:
+            deserializer.is_valid(raise_exception=True)
+        except ValidationError:
+            self.fail('Boom! You fail the test.')
+
+    def test_blank_params(self):
+        """
+        Test the deserializer with blank params
+        """
+        instance = baker.make(User, _fill_optional=True)
+
+        data = {
+            'username': '',
+            'email': '',
+            'first_name': '',
+            'last_name': '',
+        }
+
+        deserializer = self.deserializer(instance=instance, data=data)
+
+        try:
+            deserializer.is_valid(raise_exception=True)
+        except ValidationError:
+            self.fail('Boom! You fail the test.')
+
+    def test_invalid_email_param(self):
+        """
+        Test the deserializer with invalid email param
+        """
+        instance = baker.make(User)
+
+        data = {
+            'username': 'somename',
+            'email': 'invalidmail.com',
+            'first_name': 'Abra',
+            'last_name': 'Cadabra',
+        }
+
+        deserializer = self.deserializer(instance=instance, data=data)
+
+        with self.assertRaises(ValidationError) as error:
+            deserializer.is_valid(raise_exception=True)
+
+        self.assertDictEqual(
+            error.exception.detail,
+            {
+                'email': [ErrorDetail(string='Enter a valid email address.', code='invalid')]
+            }
+        )
+
+    def test_validate_1(self):
+        """
+        Test the deserializer `.validate` method with invalid password
+        """
+        old_password = 'old_password'
+
+        instance = baker.make(User)
+        instance.password = make_password(old_password)
+        instance.save()
+
+        data = {
+            'username': 'somename',
+            'email': 'somename@mail.com',
+            'old_password': old_password,
+            'new_password': '0123456',
+            'first_name': 'Abra',
+            'last_name': 'Cadabra',
+        }
+
+        deserializer = self.deserializer(instance=instance, data=data)
+
+        with self.assertRaises(DjangoValidationError) as error:
+            deserializer.validate(data)
+
+        self.assertCountEqual(
+            error.exception.messages,
+            [
+                'This password is too short. It must contain at least 8 characters.',
+                'This password is too common.',
+                'This password is entirely numeric.'
+            ]
+        )
+
+    def test_validate_2(self):
+        """
+        Test the deserializer `.validate_password` method with password's value that is too similar with `username`.
+        """
+        old_password = 'old_password'
+
+        instance = baker.make(User)
+        instance.password = make_password(old_password)
+        instance.save()
+
+        data = {
+            'email': 'Valid123@mail.com',
+            'old_password': old_password,
+            'new_password': instance.username,
+            'first_name': 'Abra',
+            'last_name': 'Cadabra',
+        }
+
+        deserializer = self.deserializer(instance=instance, data=data)
+
+        with self.assertRaises(DjangoValidationError) as error:
+            deserializer.validate(data)
+
+        self.assertListEqual(
+            error.exception.messages,
+            ['The password is too similar to the username.']
+        )
+
+    def test_validate_3(self):
+        """
+        Test the deserializer `.validate_password` method with password's value that is too similar with `email`.
+        """
+        old_password = 'old_password'
+
+        instance = baker.make(User)
+        instance.password = make_password(old_password)
+        instance.save()
+
+        data = {
+            'username': 'somename',
+            'email': 'Valid123@mail.com',
+            'old_password': old_password,
+            'new_password': 'Valid123@mail.com',
+            'first_name': 'Abra',
+            'last_name': 'Cadabra',
+        }
+
+        deserializer = self.deserializer(instance=instance, data=data)
+
+        with self.assertRaises(DjangoValidationError) as error:
+            deserializer.validate(data)
+
+        self.assertListEqual(
+            error.exception.messages,
+            ['The password is too similar to the email address.']
+        )
+
+    def test_validate_4(self):
+        """
+        Test the deserializer `.validate_password` method with password's value that is too similar with `first_name`.
+        """
+        old_password = 'old_password'
+
+        instance = baker.make(User)
+        instance.password = make_password(old_password)
+        instance.save()
+
+        data = {
+            'username': 'somename',
+            'email': 'Valid123@mail.com',
+            'old_password': old_password,
+            'new_password': 'Abra123456!',
+            'first_name': 'Abra123456!',
+            'last_name': 'Cadabra',
+        }
+
+        deserializer = self.deserializer(instance=instance, data=data)
+
+        with self.assertRaises(DjangoValidationError) as error:
+            deserializer.validate(data)
+
+        self.assertListEqual(
+            error.exception.messages,
+            ['The password is too similar to the first name.']
+        )
+
+    def test_validate_5(self):
+        """
+        Test the deserializer `.validate_password` method with password's value that is too similar with `last_name`.
+        """
+        old_password = 'old_password'
+
+        instance = baker.make(User)
+        instance.password = make_password(old_password)
+        instance.save()
+
+        data = {
+            'username': 'somename',
+            'email': 'Valid123@mail.com',
+            'old_password': old_password,
+            'new_password': 'cAdabra123456!',
+            'first_name': 'Abra',
+            'last_name': 'Cadabra123456!',
+        }
+
+        deserializer = self.deserializer(instance=instance, data=data)
+
+        with self.assertRaises(DjangoValidationError) as error:
+            deserializer.validate(data)
+
+        self.assertListEqual(
+            error.exception.messages,
+            ['The password is too similar to the last name.']
+        )
+
+    def test_validate_email(self):
+        """
+        Test the deserializer `.validate_email` method with email that is already used (case insensitive).
+        """
+        email = 'test@test.com'
+        instance = baker.make(User, email=email)
+
+        data = {
+            'username': 'somename',
+            'email': email.capitalize(),
+            'password': 'ValidSecretP4ssw0rd!',
+            'first_name': 'Abra',
+            'last_name': 'Cadabra',
+        }
+
+        deserializer = self.deserializer(instance, data=data)
+
+        with self.assertRaises(ValidationError) as error:
+            deserializer.validate_email(email.capitalize())
+
+        self.assertEqual(
+            error.exception.detail,
+            [
+                ErrorDetail(string='Email is already exists!', code='invalid')
+            ]
         )
