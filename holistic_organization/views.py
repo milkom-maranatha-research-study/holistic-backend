00340b819f85bd617c82fed2f5b236d04f0fa370
panjiyudasetya <panjiyudasetya@gmail.com>
2022-11-18 13:44:09 +0700

    Modifies `holistic_organization` app views - Add `OrganizationListView` to return list of the organizations - Add implementation details for  upserts endpoint against the `BaseSyncView`, `OrganizationSyncView`,  `TherapistOrganizationSyncView`, and  `TherapistInteractionSyncView` views

--- holistic_organization/views.py
+++ holistic_organization/views.py
@@ -1,32 +1,65 @@
+from django.http import Http404
 from drf_rw_serializers import generics
-from rest_framework import status
 from rest_framework.response import Response
 
+from holistic_organization.models import Organization
 from holistic_organization.serializers import (
+    OrganizationSerializer,
     OrganizationDeserializer,
+    SyncSerializer,
     TherapistOrganizationDeserializer,
     TherapistInteractionDeserializer
 )
 
 
+class OrganizationListView(generics.ListAPIView):
+    serializer_class = OrganizationSerializer
+    queryset = Organization.objects.all().order_by('id')
+
+
 class BaseSyncView(generics.CreateAPIView):
+    read_serializer_class = SyncSerializer
 
-    def put(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
 
-        serializer = self.get_serializer(data=request.data, many=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        deserializer = self.get_write_serializer(data=request.data, many=True)
+        deserializer.is_valid(raise_exception=True)
+        deserializer.save()
 
-        return Response(status=status.HTTP_200_OK)
+        print(deserializer.instance)
+        serializer = self.get_read_serializer(deserializer.instance)
+        return Response(serializer.data)
 
 
 class OrganizationSyncView(BaseSyncView):
-    serializer_class = OrganizationDeserializer
+    write_serializer_class = OrganizationDeserializer
 
 
 class TherapistOrganizationSyncView(BaseSyncView):
-    serializer_class = TherapistOrganizationDeserializer
+    write_serializer_class = TherapistOrganizationDeserializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['organization_id'] = self.kwargs.get('id')
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if not self.kwargs.get('id'):
+            raise Http404
+
+        return super().post(request, *args, **kwargs)
 
 
 class TherapistInteractionSyncView(BaseSyncView):
-    serializer_class = TherapistInteractionDeserializer
+    write_serializer_class = TherapistInteractionDeserializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['therapist_id'] = self.kwargs.get('id')
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if not self.kwargs.get('id'):
+            raise Http404
+
+        return super().post(request, *args, **kwargs)
