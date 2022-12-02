b3dbb5b48880a89e49c9265e8690d795141b3a2f
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-02 19:15:11 +0700

    Add a new view for exporting raw data of the therapist interaction belonging to all organization

--- holistic_organization/views.py
+++ holistic_organization/views.py
@@ -1,20 +1,92 @@
-from django.http import Http404
+import csv
+
+from django.db.models import OuterRef, Subquery
+from django.http import Http404, HttpResponse
 from drf_rw_serializers import generics
+from rest_framework import status
 from rest_framework.response import Response
 
-from holistic_organization.models import Organization
+from holistic_organization.models import (
+    Organization,
+    TherapistOrganization,
+    TherapistInteraction,
+)
 from holistic_organization.serializers import (
     OrganizationSerializer,
+    OrganizationTherapistInteractionJSONSerializer,
+    OrganizationTherapistInteractionCSVSerializer,
+    OrganizationTherapistInteractionExportDeserializer,
     OrganizationDeserializer,
     SyncSerializer,
     TherapistOrganizationDeserializer,
-    TherapistInteractionDeserializer
+    TherapistInteractionDeserializer,
 )
 
 
 class OrganizationListView(generics.ListAPIView):
     serializer_class = OrganizationSerializer
     queryset = Organization.objects.all().order_by('id')
+
+
+class OrganizationTherapistInteractionListView(generics.CreateAPIView):
+
+    def get_queryset(self):
+        ther_org_qs = TherapistOrganization.objects.filter(therapist_id=OuterRef('therapist_id'))
+
+        organization_id = Subquery(ther_org_qs.values('organization')[:1])
+        organization_date_joined = Subquery(ther_org_qs.values('date_joined')[:1])
+
+        return TherapistInteraction.objects.all()\
+            .annotate(organization_id=organization_id).annotate(organization_date_joined=organization_date_joined)
+
+    def post(self, request, *args, **kwargs):
+
+        deserializer = OrganizationTherapistInteractionExportDeserializer(data=request.data)
+        deserializer.is_valid(raise_exception=True)
+
+        format = deserializer.validated_data['format']
+        filename = 'therapists_interactions'
+
+        if format == 'json':
+            return self._get_response_json(filename)
+
+        elif format == 'csv':
+            return self._get_response_csv(filename)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def _get_response_json(self, filename: str):
+
+        serializer = OrganizationTherapistInteractionJSONSerializer(self.get_queryset(), many=True)
+
+        return Response(
+            data=serializer.data,
+            content_type='application/json',
+            headers={'Content-Disposition': f'attachment; filename="{filename}.json"'},
+        )
+
+    def _get_response_csv(self, filename: str):
+
+        response = HttpResponse(
+            content_type='text/csv',
+            headers={'Content-Disposition': f'attachment; filename="{filename}.csv"'},
+        )
+
+        headers = [
+            'interaction_id', 'therapist_id', 'chat_count', 'call_count',
+            'interaction_date', 'organization_id', 'organization_date_joined'
+        ]
+
+        rows = [
+            OrganizationTherapistInteractionCSVSerializer().to_representation(instance)
+            for instance in self.get_queryset()
+        ]
+
+        writer = csv.writer(response)
+        writer.writerow(headers)
+        writer.writerows(rows)
+
+        return response
 
 
 class BaseSyncView(generics.CreateAPIView):
