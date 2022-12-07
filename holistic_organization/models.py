f39eb29bd9f0e654eb2d92200cbf05147e3bec92
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-07 09:29:35 +0700

    Add custom queryset and model indexes - Introduce `TherapistInteractionQuerySet` - Modifies `TherapistOrganization` models and add index by (`organization_id`, `therapist_id`) - Modifies `TherapistInteraction` models and add index by (`id`, `therapist_id`) and (`therapist_id`, `interaction_date`)

--- holistic_organization/models.py
+++ holistic_organization/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import OuterRef, Subquery
 
 
 class Organization(models.Model):
@@ -22,6 +23,32 @@ class TherapistOrganization(models.Model):
         unique_together = (
             ('organization', 'therapist_id'),
         )
+        indexes = [
+            models.Index(fields=['organization_id', 'therapist_id']),
+        ]
+
+
+class TherapistInteractionQuerySet(models.QuerySet):
+
+    def annotate_organization_id(self):
+        """
+        Annotates the `self` queryset with `organization_id`.
+        """
+        queryset = TherapistOrganization.objects.filter(therapist_id=OuterRef('therapist_id'))
+        return self.annotate(organization_id=Subquery(queryset.values('organization')[:1]))
+
+    def annotate_organization_date_joined(self):
+        """
+        Annotates the `self` queryset with `organization_date_joined`.
+        """
+        queryset = TherapistOrganization.objects.filter(therapist_id=OuterRef('therapist_id'))
+        return self.annotate(organization_date_joined=Subquery(queryset.values('date_joined')[:1]))
+
+    def join_with_organization(self):
+        """
+        Annotates the `self` queryset with `organization_id` and `organization_date_joined`.
+        """
+        return self.annotate_organization_id().annotate_organization_date_joined()
 
 
 class TherapistInteraction(models.Model):
@@ -31,7 +58,13 @@ class TherapistInteraction(models.Model):
     call_count = models.PositiveIntegerField()
     interaction_date = models.DateField()
 
+    objects = TherapistInteractionQuerySet.as_manager()
+
     class Meta:
         unique_together = (
             ('interaction_id', 'therapist_id', 'interaction_date'),
         )
+        indexes = [
+            models.Index(fields=['id', 'therapist_id']),
+            models.Index(fields=['therapist_id', 'interaction_date']),
+        ]
