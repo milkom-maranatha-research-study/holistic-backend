cbcdaa073c550ba9d2a2d6c50cc6bfb361ca1ba7
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-21 15:18:26 +0700

    Adjusts organization serializers - Modifies `TherapistInteractionBatchDeserializer` to adjust organization models updates - Rename some serializers

--- holistic_organization/serializers.py
+++ holistic_organization/serializers.py
@@ -15,31 +15,23 @@ class OrganizationSerializer(serializers.ModelSerializer):
         read_only = fields
 
 
-class OrganizationTherapistInteractionJSONSerializer(serializers.ModelSerializer):
-    organization_id = serializers.SerializerMethodField()
-    organization_date_joined = serializers.SerializerMethodField()
+class JSONExportSerializer(serializers.Serializer):
 
-    class Meta:
-        model = TherapistInteraction
-        fields = (
-            'interaction_id',
-            'therapist_id',
-            'chat_count',
-            'call_count',
-            'interaction_date',
-            'organization_id',
-            'organization_date_joined',
-        )
-        read_only = fields
+    def to_representation(self, instance):
+        org_date_joined = instance.organization_date_joined
 
-    def get_organization_id(self, obj):
-        return obj.organization_id
-
-    def get_organization_date_joined(self, obj):
-        return obj.organization_date_joined
+        return {
+            "interaction_id": instance.interaction_id,
+            "therapist_id": instance.therapist_id,
+            "chat_count": instance.chat_count,
+            "call_count": instance.call_count,
+            "interaction_date": instance.interaction_date.isoformat(),
+            "organization_id": instance.organization_id,
+            "organization_date_joined": org_date_joined.isoformat() if org_date_joined else None
+        }
 
 
-class OrganizationTherapistInteractionCSVSerializer(serializers.Serializer):
+class CSVExportSerializer(serializers.Serializer):
 
     def to_representation(self, instance):
         org_date_joined = instance.organization_date_joined
@@ -55,7 +47,7 @@ class OrganizationTherapistInteractionCSVSerializer(serializers.Serializer):
         ]
 
 
-class OrganizationTherapistInteractionExportDeserializer(serializers.Serializer):
+class ExportDeserializer(serializers.Serializer):
     TYPE_JSON = 'json'
     TYPE_CSV = 'csv'
     FORMAT_CHOICES = (
@@ -119,13 +111,13 @@ class TherapistOrganizationBatchDeserializer(serializers.ListSerializer):
 
         @param therapist_list: Validated JSON Array that contains a list of therapists.
         """
-        existing_therapists = [
+        existing_org_therapists = [
             therapist
             for therapist in TherapistOrganization.objects.filter(organization_id=self.organization_id)
         ]
 
-        objects_to_update = self._get_therapists_to_update(therapist_list, existing_therapists)
-        objects_to_create = self._get_therapists_to_create(therapist_list, existing_therapists)
+        objects_to_update = self._get_therapists_to_update(therapist_list, existing_org_therapists)
+        objects_to_create = self._get_therapists_to_create(therapist_list, existing_org_therapists)
 
         TherapistOrganization.objects.bulk_update(objects_to_update, fields=['date_joined'])
         rows_created = len(TherapistOrganization.objects.bulk_create(objects_to_create))
@@ -133,70 +125,70 @@ class TherapistOrganizationBatchDeserializer(serializers.ListSerializer):
 
         return {'rows_created': rows_created, 'rows_updated': rows_updated}
 
-    def _get_therapists_to_create(self, therapist_list, existing_therapists):
+    def _get_therapists_to_create(self, therapist_list, org_therapists):
         """
         Returns a list of `TherapistOrganization` objects that are going to be created
         in batch.
 
         @param therapist_list: Validated JSON Array that contains a list of therapists.
-        @param existing_therapists: Existing therapists in the organization.
+        @param org_therapists: Existing therapists in the organization.
         """
         return [
             TherapistOrganization(
+                id=item['therapist_id'],
                 organization_id=self.organization_id,
-                therapist_id=item['therapist_id'],
                 date_joined=item['date_joined']
             )
-            for item in therapist_list if self._is_new_therapist(item, existing_therapists)
+            for item in therapist_list if self._is_new_therapist(item, org_therapists)
         ]
 
-    def _is_new_therapist(self, item, existing_therapists):
+    def _is_new_therapist(self, item, org_therapists):
         """
         Returns `True` if that therapist `item` doesn't exist in the list of existing therapists.
 
         @param item: A dictionary that represents the Therapist within the payload data.
-        @param existing_therapists: Existing therapists in the organization.
+        @param org_therapists: Existing therapists in the organization.
         """
-        for ther_org in existing_therapists:
-            if item['therapist_id'] == ther_org.therapist_id:
+        for therapist in org_therapists:
+            if item['therapist_id'] == therapist.id:
                 return False
 
         return True
 
-    def _get_therapists_to_update(self, therapist_list, existing_therapists):
+    def _get_therapists_to_update(self, therapist_list, org_therapists):
         """
         Returns a list of `TherapistOrganization` objects that are going to be updated
         in batch.
 
         @param therapist_list: Validated JSON Array that contains a list of therapists.
-        @param existing_therapists: Existing therapists in the organization.
+        @param org_therapists: Existing therapists in the organization.
         """
         therapists_to_update = []
 
         for item in therapist_list:
-            pair = self._get_pair_of_item(item, existing_therapists)
+            pair = self._get_pair_of_item(item, org_therapists)
 
             if pair is None:
                 continue
 
-            item, ther_org = list(pair)
-            ther_org.date_joined = item['date_joined']
+            item, therapist = list(pair)
+            therapist.date_joined = item['date_joined']
 
-            therapists_to_update.append(ther_org)
+            therapists_to_update.append(therapist)
 
         return therapists_to_update
 
-    def _get_pair_of_item(self, item, existing_therapists):
+    def _get_pair_of_item(self, item, org_therapists):
         """
         Returns a pair of (`item`, `TherapistOrganization`)
         if that therapist `item` exists on the list of existing therapists.
 
         @param item: A dictionary that represents the Therapist within the payload data.
-        @param existing_therapists: Existing therapists in the organization.
+        @param org_therapists: Existing therapists in the organization.
         """
-        for ther_org in existing_therapists:
-            if item['therapist_id'] == ther_org.therapist_id:
-                return (item, ther_org)
+        for therapist in org_therapists:
+            if item['therapist_id'] == therapist.id:
+                return (item, therapist)
 
         return None
 
@@ -226,6 +218,7 @@ class TherapistInteractionBatchDeserializer(serializers.ListSerializer):
 
         @param interaction_list: Validated JSON Array that contains a list of therapist's interactions.
         """
+        # 1. Find objects to be created / updated
         existing_interactions = [
             interaction
             for interaction in TherapistInteraction.objects.filter(therapist_id=self.therapist_id)
@@ -234,6 +227,29 @@ class TherapistInteractionBatchDeserializer(serializers.ListSerializer):
         objects_to_update = self._get_interactions_to_update(interaction_list, existing_interactions)
         objects_to_create = self._get_interactions_to_create(interaction_list, existing_interactions)
 
+        # 2. Special case when creating the therapist's interaction objects
+        # - We found therapist's interaction objects but they don't belongs to any Organization.
+        # - Therefore, we perform additional check and add that therapist to the `TherapistOrganization` table,
+        # - However, we leave both (`organization` and `date_joined`) to be empty.
+        unknown_ther_ids = []
+
+        if objects_to_create:
+
+            existing_ther_ids = TherapistOrganization.objects.filter(
+                id__in=[o.therapist_id for o in objects_to_create]
+            ).values_list('id', flat=True)
+
+            unknown_ther_ids = set([
+                o.therapist_id
+                for o in objects_to_create if o.therapist_id not in existing_ther_ids
+            ])
+
+        if unknown_ther_ids:
+            TherapistOrganization.objects.bulk_create(
+                [TherapistOrganization(id=ther_id)for ther_id in unknown_ther_ids]
+            )
+
+        # 3. Perform bulk operation to upsert `TherapistInteraction` objects
         TherapistInteraction.objects.bulk_update(objects_to_update, fields=['chat_count', 'call_count'])
         rows_created = len(TherapistInteraction.objects.bulk_create(objects_to_create))
         rows_updated = len(interaction_list) - rows_created
@@ -271,8 +287,8 @@ class TherapistInteractionBatchDeserializer(serializers.ListSerializer):
         for interaction in existing_interactions:
             if bool(
                 self.therapist_id == interaction.therapist_id and
-                item['interaction_id'] == interaction.interaction_id and
-                item['interaction_date'] == interaction.interaction_date
+                item['interaction_date'] == interaction.interaction_date and
+                item['interaction_id'] == interaction.interaction_id
             ):
                 return False
 
@@ -315,8 +331,8 @@ class TherapistInteractionBatchDeserializer(serializers.ListSerializer):
         for interaction in existing_interactions:
             if bool(
                 self.therapist_id == interaction.therapist_id and
-                item['interaction_id'] == interaction.interaction_id and
-                item['interaction_date'] == interaction.interaction_date
+                item['interaction_date'] == interaction.interaction_date and
+                item['interaction_id'] == interaction.interaction_id
             ):
                 return (item, interaction)
 
