109e71eb76b6d7aef66232ca4af146d6489e7b78
panjiyudasetya <panjiyudasetya@gmail.com>
2022-12-28 12:03:45 +0700

    Add all-time presentation data views

--- holistic_data_presentation/views.py
+++ holistic_data_presentation/views.py
@@ -7,16 +7,25 @@ from holistic_data_presentation.filters import (
     OrganizationRateFilter
 )
 from holistic_data_presentation.models import (
+    AllTimeNumberOfTherapist,
+    AllTimeOrganizationRate,
     NumberOfTherapist,
-    OrganizationRate
+    OrganizationRate,
 )
 from holistic_data_presentation.serializers import (
+    AllTimeNumberOfTherapistSerializer,
+    AllTimeOrganizationRateSerializer,
     BatchCreateSerializer,
     NumberOfTherapistSerializer,
-    TotalTherapistOrganizationDeserializer,
+    NumberOfTherapistDeserializer,
     OrganizationRateSerializer,
     OrganizationRateDeserializer,
 )
+
+
+class AllTimeNumberOfTherapistListView(generics.ListCreateAPIView):
+    serializer_class = AllTimeNumberOfTherapistSerializer
+    queryset = AllTimeNumberOfTherapist.objects.all().order_by('-end_date')
 
 
 class NumberOfTherapistListView(generics.ListAPIView):
@@ -27,7 +36,7 @@ class NumberOfTherapistListView(generics.ListAPIView):
 
 class NumberOfTherapistDetailView(generics.CreateAPIView):
     read_serializer_class = BatchCreateSerializer
-    write_serializer_class = TotalTherapistOrganizationDeserializer
+    write_serializer_class = NumberOfTherapistDeserializer
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
@@ -44,6 +53,11 @@ class NumberOfTherapistDetailView(generics.CreateAPIView):
 
         serializer = self.get_read_serializer(deserializer.instance)
         return Response(serializer.data)
+
+
+class AllTimeOrganizationRateListView(generics.ListCreateAPIView):
+    serializer_class = AllTimeOrganizationRateSerializer
+    queryset = AllTimeOrganizationRate.objects.all().order_by('-end_date')
 
 
 class OrganizationRateListView(generics.ListAPIView):
