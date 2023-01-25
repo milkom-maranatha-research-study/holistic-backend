520297d2957c43714b92624a7a6eb4e2ea0093fb
panjiyudasetya <panjiyudasetya@gmail.com>
2023-01-25 12:28:46 +0700

    Allows localhost to access Backend API

--- holistic_project/settings/local.py
+++ holistic_project/settings/local.py
@@ -27,6 +27,7 @@ DATABASES = {
 # django-cors-headers
 # =============================================================
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
 ]
